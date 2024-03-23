dee46bd187a862150be75174e0070377ba6c4060
Iman Moghaddas <imanmoghaddas@yahoo.com>
2024-03-22 23:25:51 -0400

    fixed the home section profile pic

--- src/components/About.js
+++ src/components/About.js
@@ -7,22 +7,22 @@ export const About = () => {
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 border-gray-500 p-2'>About</p>
         </div>
-        <div className="mt-4 md:mt-10"></div> {/* Reduce mt-10 to mt-4 on smaller screens */}
+        <div className="mt-4 md:mt-8"></div> {/* Adjust the spacing here */}
         <p className='text-xl mt-4'>
           Hi, I'm Iman Moghaddas, a 25-year-old full-stack software developer. Graduating with a Bachelor of Science degree in Software Engineering from Kennesaw State University in December 2021, 
           I've honed my skills in building comprehensive software solutions.
         </p>
-        <div className="mt-4 md:mt-10"></div> {/* Reduce mt-10 to mt-4 on smaller screens */}
+        <div className="mt-4 md:mt-8"></div> {/* Adjust the spacing here */}
         <p className='text-xl'>
           I've furthered my expertise through a full-stack web development certificate program at Georgia Tech's bootcamp. 
           This intensive training not only elevated my technical abilities but also refined my approach to problem-solving, making me a more adept and versatile developer.
         </p>
-        <div className="mt-4 md:mt-10"></div> {/* Reduce mt-10 to mt-4 on smaller screens */}
+        <div className="mt-4 md:mt-8"></div> {/* Adjust the spacing here */}
         <p className='text-xl'>
           Beyond the world of coding, I'm a passionate musician, having composed and recorded music since childhood. 
           As a singer-songwriter, I find inspiration and balance in the creative process, fueling my innovative mindset in both music and software development.
         </p>
-        <div className="mt-4 md:mt-10"></div> {/* Reduce mt-10 to mt-4 on smaller screens */}
+        <div className="mt-4 md:mt-8"></div> {/* Adjust the spacing here */}
         <p className='text-xl'>
           In my quest for continual growth, I enthusiastically embrace new challenges and opportunities to expand my horizons. 
           I thrive on the excitement of learning and mastering new activities, enriching my life with diverse experiences and knowledge.
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,44 @@
-import React from 'react';
 import profile from '../assets/profilePic.jpeg';
 import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 import { Link } from 'react-scroll';
 
 export const Home = () => {
   return (
-    <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 mt-20'>
-      <div className='max-w-screen-lg mx-auto flex flex-col md:flex-row items-center justify-center h-full px-4'>
-        <div className='flex flex-col justify-center h-full text-center md:text-left'>
-          <h2 className='text-4xl sm:text-6xl md:text-7xl font-bold text-white mb-4'>I am a Full Stack Developer</h2>
-          <p className='text-lg text-gray-500 py-2 max-w-md'>
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">
+            I'm a Full Stack Developer
+          </h2>
+          <p className="text-gray-500 py-4 max-w-md">
             Qualified and reliable professional with strong expertise in programming, technology, troubleshooting, innovation, and customer service. 
             Proficient in assisting clients and customers to organize and increase efficiency within a private organization or business. 
             Thrives in culturally diverse environments with high client satisfaction. Possesses strong administrative, organizational, and technology experience with keen attention to detail.
             Also, has a love for music, music creation/production, and entertainment, which helps keep the mind very creative, innovative, and alert.
           </p>
           <div>
-            <Link to='portfolio' smooth duration={500} className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
+            <Link
+              to="portfolio"
+              smooth
+              duration={500}
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+            >
               Portfolio
-              <span className='group-hover:rotate-90 duration-300'>
+              <span className="group-hover:rotate-90 duration-300">
                 <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
               </span>
             </Link>
           </div>
         </div>
-        <div className="mb-8">
-          <img src={profile} alt='Iman' className='rounded-2xl mx-auto w-2/3 md:w-2/3 max-w-full h-auto opacity-80'/>
+        <div>
+          <img
+            src={profile}
+            alt="my profile"
+            className="rounded-2xl mx-auto w-2/3 md:w-full"
+          />
         </div>
       </div>
     </div>
